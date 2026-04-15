17bb39c1781d7b626a461d4c1f4002f4d24e87bc
Marko Mijatovic <mij220034@spengergasse.at>
2026-04-15 14:14:54 +0200

    0.1

---
+++ src/main/java/at/spengergasse/taptrack/TapTrackApplication.java
@@ -0,0 +1,13 @@
+package at.spengergasse.taptrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapTrackApplication.class, args);
+    }
+
+}
